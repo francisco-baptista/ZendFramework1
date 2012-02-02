616241650fad8b2072f3a89ed597040e01aa492f
Francisco Baptista <francisco@tomboa.net>
2012-02-02 13:02:47 +0000

    setup paths and load zend application

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+// Define path for Zend 1.11.11 and Doctrine2 libraries
+ini_set("include_path", ".:" . realpath(dirname(__FILE__)) .
+	"/../Zend/:" . realpath(dirname(__FILE__)) . "/../Doctrine/:" . PEAR_INSTALL_DIR);
 
 // Define path to application directory
 defined('APPLICATION_PATH')
@@ -6,7 +9,7 @@ defined('APPLICATION_PATH')
 
 // Define application environment
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
@@ -19,8 +22,9 @@ require_once 'Zend/Application.php';
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
-    APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
+	APPLICATION_ENV,
+	APPLICATION_PATH . '/configs/application.json'
 );
+
 $application->bootstrap()
-            ->run();
+	->run();
